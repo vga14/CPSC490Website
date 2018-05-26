6b87f2dcea638e35e51249dbf6e40d64dd9c08e7
vahid <vahidgolchin90@gmail.com>
2018-05-26 00:14:32 -0700

    version 2

--- functions.js
+++ functions.js
@@ -1,17 +1,28 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
 
-// Get the header
-var header = document.getElementById("myHeader");
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+  // Get todays date and time
+  var now = new Date().getTime();
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in an element with id="demo"
+  document.getElementById("counter").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "EXPIRED";
   }
-}
+}, 1000);
